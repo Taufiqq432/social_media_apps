7b44a8b621cfd4f3ac9bc618387490c977889f38
Trust Computer <taufiqelahiiceiu@gmail.com>
2023-05-12 12:15:14 +0600

    final

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -17,28 +17,32 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          InputTextField(
-              mycontroller: emailcontroller,
-              focusnode: emailfocusnode,
-              onfieldsubmited: (value){},
-              keyBordType: TextInputType.emailAddress,
-              obsecureText: false,
-              hint: 'Email',
-              validator: (value){
-                return value.isEmpty?'enter email':null;
-              }
-          ),
-          const SizedBox(height: 40),
-          RoundButton(
-            title: 'Login',
-            color: AppColors.secondaryColor,
-            onPress: () {},
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InputTextField(
+                mycontroller: emailcontroller,
+                focusnode: emailfocusnode,
+                onfieldsubmited: (value){},
+                keyBordType: TextInputType.emailAddress,
+                obsecureText: false,
+
+                hint: 'Email',
+                validator: (value){
+                  return value.isEmpty?'enter email':null;
+                }
+            ),
+            const SizedBox(height: 40),
+            RoundButton(
+              title: 'Login',
+              color: AppColors.secondaryColor,
+              onPress: () {},
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/wigets/input_text_field.dart
+++ lib/wigets/input_text_field.dart
@@ -10,6 +10,7 @@ class InputTextField extends StatelessWidget {
   final String hint;
   final bool obsecureText;
   final bool enable, autoFocus;
+  final String labelText;
   const InputTextField(
       {Key? key,
       required this.mycontroller,
@@ -20,39 +21,54 @@ class InputTextField extends StatelessWidget {
       required this.hint,
       required this.validator,
       this.enable = true,
-      this.autoFocus = false})
+      this.autoFocus = false,
+      this.labelText=''})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      cursorColor: AppColors.dialogAlertBackgroundColor,
-      controller: mycontroller,
-      focusNode: focusnode,
-      onFieldSubmitted: onfieldsubmited,
-      validator: validator,
-      keyboardType: keyBordType,
-      style: Theme.of(context).textTheme.bodyText2,
-      obscureText: obsecureText,
-      decoration: InputDecoration(
-          hintText: hint,
-          enabled: enable,
-          contentPadding: EdgeInsets.all(15),
-          hintStyle: Theme.of(context)
-              .textTheme
-              .bodyText2!
-              .copyWith(color: AppColors.primaryTextTextColor.withOpacity(0.8)),
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: AppColors.textFieldDefaultFocus),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: TextFormField(
+        cursorColor: AppColors.dialogAlertBackgroundColor,
+        controller: mycontroller,
+        focusNode: focusnode,
+        onFieldSubmitted: onfieldsubmited,
+        validator: validator,
+        keyboardType: keyBordType,
+        style: Theme.of(context).textTheme.bodyText2!.copyWith(height:0,fontSize: 20),
+        obscureText: obsecureText,
+        decoration: InputDecoration(
+            hintText: hint,
+            labelText: labelText,
+            enabled: enable,
+            contentPadding: EdgeInsets.all(15),
+            hintStyle: Theme.of(context)
+                .textTheme
+                .bodyText2!
+                .copyWith(height:0,color: AppColors.primaryTextTextColor.withOpacity(0.8)),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: AppColors.textFieldDefaultFocus),
+
+            ),
+          focusedBorder:  OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColors.secondaryColor),
 
           ),
-        focusedBorder:  OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(color: AppColors.secondaryColor),
+          errorBorder:  OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColors.alertColor),
+
+          ),
+          enabledBorder:  OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColors.textFieldDefaultBorderColor),
+
+          ),
 
         ),
-
       ),
     );
   }
