8c90d305607c2018d9b58ad9abad4882cbdd7fa1
Trust Computer <taufiqelahiiceiu@gmail.com>
2023-05-20 01:20:32 +0600

    final register user

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:social_media_app/component/color.dart';
 class Utils{
   static void fieldFocus(BuildContext context,FocusNode currentnode,FocusNode nextnode){
     currentnode.unfocus();
     FocusScope.of(context).requestFocus(nextnode);
+  }
+  static void toastMessage(String message){
+    Fluttertoast.showToast(
+        msg: message,
+      backgroundColor: AppColors.primaryTextTextColor,
+      textColor: AppColors.whiteColor,
+      fontSize: 14
+    );
+
   }
 
 }
